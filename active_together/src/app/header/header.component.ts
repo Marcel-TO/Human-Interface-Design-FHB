2a09d4c380917db84b3780ae67315acfbd5c6086
Marcel T.O <marcel@turobin-ort.at>
2024-12-20 19:17:57 +0100

    Refactor header component to use Angular Material toolbar and menu, update styles for improved layout

--- active_together/src/app/header/header.component.ts
+++ active_together/src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatIconModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   public title: string = 'Stay Active, Stay Together';
-  public imagePath: string = "./../assets/images/sport.jpeg";
 }
